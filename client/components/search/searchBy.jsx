86e827bc005b8bc9d7b4d04fc863c677ffbe96b0
Your Name <you@example.com>
2018-11-15 09:49:59 +1300

    search and map updated

---
+++ client/components/search/searchBy.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import SearchBar from 'material-ui-search-bar';
+import Script from 'react-load-script';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      query: ''
+    };
+
+    this.handleScriptLoad = this.handleScriptLoad.bind(this);
+    this.handlePlaceSelect = this.handlePlaceSelect.bind(this);
+
+  }
+
+  handleScriptLoad() {
+    // Declare Options For Autocomplete
+    var options = {
+      types: ['(cities)'],
+    };
+
+    // Initialize Google Autocomplete
+    /*global google*/ // To disable any eslint 'google not defined' errors
+    this.autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('autocomplete'),
+      options,
+    );
+
+    // Fire Event when a suggested name is selected
+    this.autocomplete.addListener('place_changed', this.handlePlaceSelect);
+  }
+  
+  handlePlaceSelect() {
+
+    // Extract City From Address Object
+    let addressObject = this.autocomplete.getPlace();
+    let address = addressObject.address_components;
+
+    // Check if address is valid
+    if (address) {
+      // Set State
+      this.setState(
+        {
+          city: address[0].long_name,
+          query: addressObject.formatted_address,
+        }
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Script
+          url="https://maps.googleapis.com/maps/api/js?key=your_api_key&libraries=places"
+          onLoad={this.handleScriptLoad}
+        />
+        <SearchBar id="autocomplete" placeholder="" hintText="Search City" value={this.state.query}
+          style={{
+            margin: '0 auto',
+            maxWidth: 800,
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBy;
