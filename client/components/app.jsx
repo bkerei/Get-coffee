250b3ea533b26b951b847cc924252e534ea8dd49
Steeeeeve <Steeeeeve@Steeeeeve.ste>
2018-11-15 15:21:42 +1300

    pushing in case computer fucks up

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 
 import form from './form'
-import map from './map'
+import map from './map/map'
 
 const App = ({children}) => (
   <div className="container">
